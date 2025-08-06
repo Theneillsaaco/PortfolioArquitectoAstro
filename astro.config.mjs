6dbc0cf97d770226fa11cf3e39178d3497aa0405
Theneillsaaco <isaacdepena18@gmail.com>
2025-08-06 12:09:46 -0400

    Add github deploy update index update font

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://theneillsaaco.github.io',
     server:{
         host: true,
         port: 4321,
