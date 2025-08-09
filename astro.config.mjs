1c4b7a209f7081f475e75a3f26b630bc3ccab4db
Theneillsaaco <isaacdepena18@gmail.com>
2025-08-08 21:00:32 -0400

    Refactor code design improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Theneillsaaco.github.io',
-    base: '/PortfolioArquitectoAstro',
-    server:{
-        host: true,
-        port: 4321,
-    },
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    compressHTML: true,
+  site: 'https://Theneillsaaco.github.io',
+  base: '/PortfolioArquitectoAstro',
+
+  server:{
+      host: true,
+      port: 4321,
+  },
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  compressHTML: true,
+  integrations: [icon()],
 });
