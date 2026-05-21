30e3f3bc6a560f299b9cb5d0ee7f813e587f490e
Theneillsaaco <isaacdepena18@gmail.com>
2026-05-21 17:10:11 -0400

    Update Astro dependencies and project slugs
    This commit updates various Astro dependencies to their latest versions,
    including `@astrojs/check`, `astro`, and others. It also refactors
    project slugs to use `project.id` instead of `project.slug` across the
    project pages and components. This change ensures that the correct
    project identifier is used for navigation and transitions, improving the
    overall stability and maintainability of the project.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,12 @@
-﻿import { defineCollection, z } from 'astro:content';
+﻿import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
+  loader: glob({
+    pattern: "**/[^_]*.{md,mdx}",
+    base: "./src/content/projects",
+    }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
